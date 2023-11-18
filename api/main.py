9409752eba03124bf3a60c8c54875884ef2ad970
shenziel <24pedroluis7.95@gmail.com>
2023-11-18 14:44:29 +0000

    added request to the unsplash api

--- api/main.py
+++ api/main.py
@@ -1,11 +1,32 @@
-# save this as app.py
-from flask import Flask
+import os
+import requests
+from flask import Flask, request
+from dotenv import dotenv_values
 
+config = {
+    **os.environ,  # override loaded values with environment variables
+    **dotenv_values(".env.local")
+}
+
+UNSPLASH_URL="https://api.unsplash.com/photos/random"
+UNSPLASH_KEY=config.get("UNSPLASH_KEY")
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please create .env.local file and insert there UNSPLASH_KEY value")
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers = {
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID " + UNSPLASH_KEY
+    }
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+
+    return data
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => (
   <div className="mt-4 p-5 rounded">
     <h1>Images Gallery</h1>
     <p>
-      This is a simple application that retrieves photos ising Unsplash API. In
+      This is a simple application that retrieves photos using Unsplash API. In
       order to start enter any search term in the input field
     </p>
     <p>
